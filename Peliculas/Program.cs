b0f80e6ce89935ff26a8f6cc41759c5f0179a2fa
SantiagoYpes <santiyezu8@hotmail.com>
2026-03-03 18:02:15 -0500

    relaciones clases

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -3,14 +3,20 @@ Console.WriteLine("Hola Bienvenido a nuestra tienda de alquier y ventas de pelic
 
 
 
-public class clientes {
+
+
+public class Clientes {
 public int Id  { get; set; }
 public string ? Nombre  { get; set; } 
 public string ? Cedula   { get; set; } 
 public String ? Correo  { get; set; }  
 public DateTime Fecha   { get; set; } 
 public string? Telefono   { get; set; } 
+
+public List<Facturas>? Facturas { get; set; }
+public List<Devoluciones>? Devoluciones { get; set; }
 };
+
 
 public class Peliculas {
 public int Id  { get; set; }
@@ -20,6 +26,11 @@ public string? Clasi_edad { get; set; }
 public int puntuacion  { get; set; } 
 public bool Disponibilidad  { get; set; } 
 public List < TiposGeneros>? TiposGeneros { get; set; }
+public List<Repartos>? Repartos { get; set; }
+ public List<Formatos_Peliculas>? Formatos_Peliculas { get; set; }
+public List<Inventarios>? Inventarios { get; set; }
+ public List<Ventas_Peliculas>? Ventas_Peliculas { get; set; }
+ public List<Rentas_Peliculas>? Rentas_Peliculas { get; set; }
 
 };
 
@@ -28,6 +39,7 @@ public class TiposGeneros
     public int Id  { get; set; }
 
     public string ? Genero { get; set; }
+     public Peliculas? Pelicula { get; set; }
 
 }
 
@@ -50,6 +62,11 @@ public DateTime Fecha   { get; set; }
  public int Id_Ventas { get; set; }
  public int Id_Descuentos { get; set; }
  public decimal Total { get; set; } = 0.0m ;
+ public Clientes? Cliente { get; set; }
+ public Rentas? Renta { get; set; }
+  public Ventas? Venta { get; set; }
+public List<Reclamos>? Reclamos { get; set; }
+public List<Devoluciones>? Devoluciones { get; set; }
 
 }
 
@@ -60,6 +77,8 @@ public decimal Precio { get; set; } = 0.0m ;
 public  int Cantidad { get; set; } 
 public DateTime Fecha_Renta  { get; set; } 
 public DateTime Fecha_Limite { get; set; } 
+ public List<Rentas_Peliculas>? Rentas_Peliculas { get; set; }
+ public List<Facturas>? Facturas { get; set; }
 }
 
 
@@ -67,6 +86,9 @@ public class Ventas {
 public int Id  { get; set; }
 public decimal Precio { get; set; } = 0.0m ;
 public  int Cantidad { get; set; } 
+
+public List<Ventas_Peliculas>? Ventas_Peliculas { get; set; }
+public List<Facturas>? Facturas { get; set; }
 
 }
 
@@ -78,6 +100,8 @@ public string? Nacionalidad { get; set; }
 public string? Premios { get; set; }
 public  int CantidadP { get; set; } 
 
+public List<Repartos>? Repartos { get; set; }
+
 }
 
 
@@ -88,6 +112,9 @@ public int Id_Peliculas  { get; set; }
 public string? Personaje { get; set; }
 public string? Rol { get; set; }
 
+public Actores? Actor { get; set; }
+public Peliculas? Pelicula { get; set; }
+
 }
 
 public class Sucursales  {
@@ -97,6 +124,8 @@ public string? Nombre { get; set; }
 public string? Ciudad { get; set; }
 public string? Direccion { get; set; }
 public int Telefono { get; set; }
+public List<Empleados>? Empleados { get; set; }
+public List<Inventarios>? Inventarios { get; set; }
 
 }
 
@@ -108,6 +137,7 @@ public string? Ciudad { get; set; }
 public string? Correo { get; set; }
 public int Telefono { get; set; }
 public string? Cargo { get; set; }
+public Sucursales? Sucursal { get; set; }
 
 }
 
@@ -118,6 +148,7 @@ public string? Formato { get; set; }
 public string? Idioma { get; set; }
 public bool Subtitulada { get; set; }
 public bool Disponible  { get; set; }
+ public List<Formatos_Peliculas>? Formatos_Peliculas { get; set; }
 
 }
 
@@ -129,6 +160,9 @@ public int Id_Peliculas{ get; set; }
 public int Id_Fomatos { get; set; }
 public int Id_Invetarios{ get; set; }
 public decimal Precio { get; set; } = 0.0m ;
+public Peliculas? Pelicula { get; set; }
+ public Formatos? Formato { get; set; }
+ public Inventarios? Inventario { get; set; }
 
 }
 
@@ -141,7 +175,9 @@ public int Id_Peliculas{ get; set; }
 public int Cantidad { get; set; }
 public string? Id_Formatos { get; set; }
 public string? Id_Sucursal { get; set; }
-
+public Peliculas? Pelicula { get; set; }
+public Formatos? Formato { get; set; }
+public Sucursales? Sucursal { get; set; }
 
 }
 
@@ -155,7 +191,8 @@ public int Id_Ventas { get; set; }
 public int Id_Peliculas{ get; set; }
 public int Cantidad { get; set; }
 public decimal Precio { get; set; } = 0.0m ;
-
+public Ventas? Venta { get; set; }
+public Peliculas? Pelicula { get; set; }
 }
 
 public class Rentas_Peliculas   {
@@ -166,6 +203,8 @@ public int Id_Peliculas{ get; set; }
 public int Cantidad { get; set; }
 public int Dias { get; set; }
 public decimal Total { get; set; } = 0.0m ;
+public Rentas? Renta { get; set; }
+public Peliculas? Pelicula { get; set; }
 
 }
 
@@ -178,6 +217,9 @@ public int Id_Peliculas{ get; set; }
 public int Id_Facturas { get; set; }
 public string? Multa { get; set; }
 public decimal Precio_Multa { get; set; } = 0.0m ;
+ public Clientes? Cliente { get; set; }
+ public Peliculas? Pelicula { get; set; }
+ public Facturas? Factura { get; set; }
 
 }
 
@@ -189,5 +231,6 @@ public int Id_Facturas { get; set; }
 public string? Motivo { get; set; }
 public DateTime Fecha { get; set; } 
 public string? Garantia { get; set; }
+public Facturas? Factura { get; set; }
 
 }
